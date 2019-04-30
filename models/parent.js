2b27318b047c51393c54f4116365d7b0ccaa3fa5
kchoi123 <k3vinch0i@gmail.com>
2019-04-30 15:34:53 -0700

    parents model validated with team

---
+++ models/parent.js
@@ -0,0 +1,57 @@
+// Creating Activity model for activities table
+module.exports = function(sequelize, DataTypes) {
+  var Parent = sequelize.define("parents", {
+    userName: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: true,
+      validate: {
+        len: [5, 10]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [5, 20]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      isEmail: true, // checks for email format (test@test.com)
+      validate: {
+        notEmpty: true
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      is: ["^[a-z]+$", "i"], // will only allow letters
+      allowNull: true
+    },
+    state: {
+      type: DataTypes.STRING,
+      is: ["^[a-z]+$", "i"], // will only allow letters
+      allowNull: true
+    }
+  });
+
+  Parent.associate = function(models) {
+    Parent.hasMany(models.posts, {
+      as: "posts",
+      foreignKey: "parentId"
+    });
+    Parents.belongsToMany(models.school, {
+      as: "school",
+      through: "parentSchools",
+      foreignKey: "parentId"
+    });
+    Parents.hasMany(models.comment, {
+      as: "comment",
+      foreignKey: "parentId"
+    });
+  };
+
+  return Parent;
+};

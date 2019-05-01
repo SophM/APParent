0c60671d431c797419812f67299fdb9985324f9b
yusungsamuel <ssyyu@ucdavis.edu>
2019-04-30 21:12:40 -0700

    update passport file so that it matches with parent model. update parent model to all datatype are correct. update school model so it return school

--- models/Parent.js
+++ models/Parent.js
@@ -2,7 +2,7 @@
 module.exports = function(sequelize, DataTypes) {
   var Parent = sequelize.define("parents", {
     userName: {
-      type: Sequelize.STRING,
+      type: DataTypes.STRING,
       unique: true,
       allowNull: true,
       validate: {

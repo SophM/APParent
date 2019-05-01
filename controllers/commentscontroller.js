551cd610964f9c989a180d3d9c754205f01ac88d
yusungsamuel <ssyyu@ucdavis.edu>
2019-05-01 14:35:12 -0700

    add create comment function to comment controller

--- controllers/commentscontroller.js
+++ controllers/commentscontroller.js
@@ -2,9 +2,23 @@ var db = require("../models")
 
 module.export = {
     findAllForPost: function(req, res){
-        db.comments.findAll({where:{postId: req.params.id}})
+        db.comments.findAll({
+            where:{
+                postId: req.params.id
+            }
+        })
         .then(function(result){
             res.json(result)
+        });
+    },
+    create: function(req, res){
+        db.comment.create({
+            description: req.body.description
+        }).then(function(){
+            //This needs to be change to res.redirect(/api/post/allcomments) when the route is define
+            res.end()
         })
     }
+
+    //can add remove comment function as a stretch goal
 }

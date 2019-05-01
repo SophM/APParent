0c0ec6df43a580f46e9fc19289ab1bea8119ecb5
SophieM <47410186+SophM@users.noreply.github.com>
2019-05-01 14:41:02 -0700

    add seeds files for parents and kids

---
+++ db/seeds-parents.sql
@@ -0,0 +1,8 @@
+USE apparentDB;
+
+INSERT INTO parents (userName, password, email, city, state, createdAt, updatedAt)
+
+VALUES
+("Best Parent Ever", "cou234cou567", "blabla@blabla.com", "San Francisco", "CA", "2019-04-12 03:31:57", "2019-04-12 03:31:57"),
+("The Boss", "chantilly123", "trucs@blabla.com", "Minneapolis", "MN", "2019-04-13 03:31:57", "2019-04-12 03:31:57"),
+("Busy Parent", "hello@#$you", "donuts@blabla.com", "Cupertino", "CA", "2019-04-14 03:31:57", "2019-04-12 03:31:57");

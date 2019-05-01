3d3f0b9c3434db444dce0bfa5032192cf329e75c
yusungsamuel <ssyyu@ucdavis.edu>
2019-05-01 14:22:11 -0700

    add findAllForPost method to commentcontroller for finding all comments to a specific post

--- controllers/commentscontroller.js
+++ controllers/commentscontroller.js
@@ -1,1 +1,10 @@
-const db = require("../models")
+var db = require("../models")
+
+module.export = {
+    findAllForPost: function(req, res){
+        db.comments.findAll({where:{postId: req.params.id}})
+        .then(function(result){
+            res.json(result)
+        })
+    }
+}

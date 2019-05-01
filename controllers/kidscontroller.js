d7ab11bbc0fac8c7db6258549a04fd7b3eaccec3
yusungsamuel <ssyyu@ucdavis.edu>
2019-05-01 14:58:17 -0700

    add create kid function in kid controller

--- controllers/kidscontroller.js
+++ controllers/kidscontroller.js
@@ -1,1 +1,18 @@
-const db = require("../models")
+var db = require("../models")
+
+module.export = {
+    create: function(req, res, parentId){
+        db.kids.create({
+            name: req.body.name,
+            gradeLevel: req.body.gradeLevel,
+            //get parentId from the result of creating a parent row
+            parentId:parentId, 
+            schoolId: req.body.school
+        }).then(function(result){
+            //This needs to be change to res.redirect(/api/parents/dashboard) once the route is define
+            res.end()
+        })
+    }
+
+    //update kids info function as a stretch goal
+}

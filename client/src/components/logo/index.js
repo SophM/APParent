f044a4ed6cb5124ef0d889b98107c3e950acc6f6
Namita Shenai <namita.shenai@gmail.com>
2019-05-02 13:31:10 -0700

    Adding lgo file & react logo component

---
+++ client/src/components/logo/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+function Logo() {
+    return (
+        <div className="logo">
+            <img src="public/images/logo-only-color.png"/>
+        </div>
+    )
+}
+
+export default Logo;
+  

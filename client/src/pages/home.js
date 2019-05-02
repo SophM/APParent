177cc43e33b9094e5df10c89613ae6c64d0410c5
kchoi123 <k3vinch0i@gmail.com>
2019-05-02 13:35:47 -0700

    get dom ready for component build

---
+++ client/src/pages/home.js
@@ -0,0 +1,22 @@
+// this is the landing page
+
+import React, { Component } from "react";
+import dashboard from "../components/SearchCard";
+import login from "../components/SearchResult";
+import signup from "../components/BookItemCard";
+import API from "../utils/API";
+
+
+class Books extends Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default home;

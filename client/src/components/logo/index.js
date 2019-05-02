332822141bfe24aa1c35b56fb4f1cf300ba1bf5d
kchoi123 <k3vinch0i@gmail.com>
2019-05-02 13:51:49 -0700

    update nav bar with template

--- client/src/components/logo/index.js
+++ client/src/components/logo/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Logo() {
     return (
         <div className="logo">
-            <img src="public/images/logo-only-color.png"/>
+            <img src="./images/logo-only-color.png"/>
         </div>
     )
 }

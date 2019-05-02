9d6465d37984a31ea8eb6cd48786503dcab99c85
SophieM <47410186+SophM@users.noreply.github.com>
2019-05-02 14:29:19 -0700

    add code to home page, modify navbar, modify footer, delete logo component because we are not using it

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 function Footer() {  
     return (
-        <div id="footer" class="jumbotron jumbotron-fluid pt-0 pb-0 mb-0">
-            <div class="container pt-4 text-center">
-                <small class="text-white align-bottom">Made with fun by Namita, Sophie, Kevin & Samuel</small>
+        <div id="footer" className="jumbotron jumbotron-fluid pt-0 pb-0 mb-0">
+            <div className="container pt-4 text-center">
+                <small className="text-white align-bottom font-weight-bold">Made with fun by Namita, Sophie, Kevin & Samuel</small>
             </div>
         </div>
     );
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,22 +1,24 @@
 // this is the landing page
-
 import React, { Component } from "react";
-import dashboard from "../components/SearchCard";
-import login from "../components/SearchResult";
-import signup from "../components/BookItemCard";
-import API from "../utils/API";
 
+class Home extends Component {
 
-class Books extends Component {
+    state = {
+        
+    };
 
     render() {
         return (
             <div>
-                
+                <h1 className="text-center mt-4">Welcome to APP@rent! Sign-up or Login!</h1>
+                <div className="container text-center">
+                    <button className="btn btn-lg btn-warning m-2">Sign-up</button>
+                    <button className="btn btn-lg btn-success m-2">Login</button>
+                </div>
             </div>
         )
     }
 
 }
 
-export default home;
+export default Home;

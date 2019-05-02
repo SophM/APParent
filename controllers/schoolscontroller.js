c727addb6c5a2b9e8d41c5a8a4f0d14c762f9d30
yusungsamuel <ssyyu@ucdavis.edu>
2019-05-01 19:41:28 -0700

    add create function to school model

--- controllers/schoolscontroller.js
+++ controllers/schoolscontroller.js
@@ -6,5 +6,16 @@ module.export = {
         .then(function(result){
             res.json(result)
         })
+    }, 
+    create : function(req, res){
+        db.school.create({
+            name: req.body.name,
+            streetAddress: req.body.streetAddress,
+            city: req.body.city,
+            state: req.body.state,
+            zipcode: req.body.zipcode
+        }).then(function(result){
+            res.json(result)
+        })
     }
 }

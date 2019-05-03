05693ac3198835d5b0fc9ba8c36c673f641ad148
SophieM <47410186+SophM@users.noreply.github.com>
2019-05-03 16:11:00 -0700

    add styling to allMember and userCard components so it renders more nicely

--- client/src/components/allMembers/index.js
+++ client/src/components/allMembers/index.js
@@ -2,10 +2,14 @@ import React from "react";
 
 function AllMembers({children}) {  
     return (
-      <div className="container-fluid mt-3">
+      <div className="container mt-4">
         <div className="card">
           <h5 className="card-header">All Members</h5>
-          <div className="card-body">{children}</div>
+          <div className="card-body">
+            <div className="card-columns">
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     );
--- client/src/components/userCard/index.js
+++ client/src/components/userCard/index.js
@@ -1,22 +1,44 @@
-import React from "react"; 
+import React from "react";
 
-function userCard(props) { 
+function userCard(props) {
 
     return (
-        <div className="card-columns">
-          
-                    <img className="card-img" src="http://lorempixel.com/600/500/people" //{props.thumbnail} 
-                        alt={props.userName} style={{ width: "300px" }} />
-              
-                <div className="card-body">
-                    <h4 class="card-title">{props.userName}</h4>
-                    <p className="card-text">Email: {props.email}</p>
-                    <p className="card-text">City: {props.city} | State : {props.state}</p>
-                </div>
-                <a href="#" target="_blank" className="btn round btn-primary" role="button">See Profile</a>
-           
-        </div> 
+        // <div className="card mb-3">
+        //     <div className="row no-gutters">
+        //         <div className="col">
+        //             <div className="card-body">
+        //                 <img className="img-thumbnail img-fluid" src="http://lorempixel.com/600/500/people" //{props.thumbnail} 
+        //                     alt={props.userName} style={{ width: "150px", float: "left" }} />
+
+        //                 <div className="card-body float-left text-left pt-1">
+        //                     <h2 class="card-title">{props.userName}</h2>
+        //                     <h4 className="card-text">Email: {props.email}</h4>
+        //                     <h4 className="card-text">City: {props.city} | State : {props.state}</h4>
+        //                 </div>
+
+        //                 <button className="btn btn-primary btn-lg float-right mt-5 mr-5">
+        //                     Chat
+        //                 </button>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+
+
+
+        <div className="card" style={{ width: "18rem" }}>
+            <img src="http://lorempixel.com/600/500/people" className="card-img-top" alt={props.userName} />
+            <div className="card-body">
+                <h2 className="card-title">{props.userName}</h2>
+                <h5 className="card-text">Email: {props.email}</h5>
+                <h5 className="card-text">City: {props.city}</h5>
+                <h5 className="card-text">State : {props.state}</h5>
+                <button className="btn btn-primary btn-lg mt-1">Chat</button>
+            </div>
+        </div>
+
     );
 }
 
-export default userCard; 
+export default userCard;

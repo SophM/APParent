d9f6df14ef4031b449a858d3a04501f474ee0c11
Kevin <k3vinch0i@gmail.com>
2019-05-02 21:49:35 -0700

    dashboard changes

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,26 +1,37 @@
 // this is the main dashbaord after the user login
 import React, { Component } from "react";
-import Sidebar  from "../components/sidebar";
+import Sidebar from "../components/sidebar";
+import Activity from "../components/activity";
+import PostCard from "../components/postCard";
 
 class Dashboard extends Component {
+  state = {
+    username: "Sophie, Namita , Samuel & Kevin"
+  };
 
-    state = {
-     username: 'Sophie, Namita , Samuel & Kevin'
-    };
+  render() {
+    return (
+      <div>
+        <Sidebar />
+        <h1 className="text-center mt-4">Welcome {this.state.userName}</h1>
 
-    render() {
-        return (
-            <div>
-                <Sidebar /> 
-                <h1 className="text-center mt-4">Welcome {this.state.userName}</h1> 
+        <div className="container text-center" />
 
-                <div className="container text-center"> 
-                </div>
-                   
-            </div>
-        )
-    }
+        {/* =====================================KCKCKC======================================================== */}
 
+        {/* This is the activity section */}
+        <Activity>
+
+            <PostCard />
+
+            <h3>No Results to Display</h3>
+
+        </Activity>
+
+        {/* =====================================KCKCKC======================================================== */}
+      </div>
+    );
+  }
 }
 
 export default Dashboard;

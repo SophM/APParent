3a2a7cd7f270eb8be8c3dc171c293e5377e6439e
kchoi123 <k3vinch0i@gmail.com>
2019-05-04 12:29:55 -0700

    update model on comment

--- client/src/components/postCard/index.js
+++ client/src/components/postCard/index.js
@@ -3,20 +3,57 @@ import "./style.css";
 
 function PostCard(props) {
   return (
-    <div className="card mb-3">
-      <div className="row no-gutters">
-        <div className="col">
-          <div className="card-body">
-            <h1 className="card-title">{props.title}</h1>
-            <p className="category">Category: {props.category}</p>
-            <p className="card-text">Post By: {props.name}</p>
-            <p className="card-text">{props.description}</p>
+    <div>
+      
+      <div className="card mb-3">
+        <div className="row no-gutters">
+          <div className="col">
+            <div className="card-body">
+              <h1 className="card-title">{props.title}</h1>
+              <p className="category">Category: {props.category}</p>
+              <p className="card-text">Post By: {props.name}</p>
+              <p className="card-text">{props.description}</p>
+            </div>
+            <button
+              className="btn btn-primary btn-lg float-right m-3 mr-5"
+              id="comment"
+              data-toggle="modal"
+              data-target="#exampleModalLong"
+              onClick="id01"
+            >
+              Comment
+            </button>
           </div>
-          <button className="btn btn-primary btn-lg float-right m-3 mr-5"id="comment" data-toggle="modal" data-target="#exampleModalLong">
-          Comment
-          </button>
         </div>
       </div>
+
+      {/* modal */}
+      <div
+        className="modal fade"
+        id="exampleModalLong"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLongTitle"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              Title
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">test</div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }

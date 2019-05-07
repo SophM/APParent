14d108b1ccd520b6686d794525aff0a6af83fb78
kchoi123 <k3vinch0i@gmail.com>
2019-05-06 17:53:41 -0700

    restructuring seed for demo day

--- db/seeds-posts.sql
+++ db/seeds-posts.sql
@@ -3,6 +3,51 @@ USE apparentDB;
 INSERT INTO posts (title, description, category, createdAt, updatedAt, parentId)
 
 VALUES
-("Votre avis!", "Eh je pense que les enfants doivent aller au lit vers 20h! Q'en pensez-vous ?", "Question", "2019-04-12 03:31:57", "2019-04-12 03:31:57", 3),
-("Available", "I am free tomorrow around 3pm if someone needs help!", "Offer", "2019-04-12 03:31:57", "2019-04-12 03:31:57", 2),
-("Things to do this weekend", "Check this link out if you want to have fun this weekend with your kids: https://zenhabits.net/100-ways-to-have-fun-with-your-kids-for/", "Event", "2019-04-12 03:31:57", "2019-04-12 03:31:57", 1);
+("Selling Xbox", 
+	"My son is playing too much Xbox we are selling it. Giving it away for $10. Anyone interested?", 
+	"On Sale", 
+	"2019-05-06 03:31:57", 
+	"2019-05-06 03:31:57", 
+	1),
+
+("Annoying kid!", 
+	"My kid is so annoying what do I do...? He's 17 and acts like he's 7. He doesn't stop asking questions and he's just always there!!! Someone please take him away!", 
+	"Question", 
+	"2019-05-06 03:31:57", 
+	"2019-05-06 03:31:57", 
+	1),
+
+("I am available to pick up kids", 
+	"I am free tomorrow around 3pm if someone needs help!", 
+	"Offer", 
+	"2019-05-05 03:31:57", 
+	"2019-05-05 03:31:57",  
+	2),
+
+("Things to do this weekend", 
+	"Check this link out if you want to have fun this weekend with your kids: https://zenhabits.net/100-ways-to-have-fun-with-your-kids-for/", 
+	"Event", 
+	"2019-04-12 03:31:57", 
+	"2019-04-12 03:31:57", 
+	3),
+
+("Can someone help pick up my son from school?", 
+	"Parents that live in Richmond district, San Francisco, do you have time around 10-11pm to pick up my son Shayan?", 
+	"Question", 
+	"2019-04-12 03:31:57", 
+	"2019-04-12 03:31:57", 
+	4),
+
+("Giving away baby STUFF!", 
+	"Our baby is now 18 years old. She is no longer a baby and will not need these things. Please PM me and take this from us! THANK YOU!", 
+	"Free", 
+	"2019-04-12 03:31:57", 
+	"2019-04-12 03:31:57", 
+	5),
+
+("Found a great website for parents", 
+	"Found a pretty cool website for kids here: https://www.bayareakidfun.com/family-friendly-events-in-the-bay-area/", 
+	"Event", 
+	"2019-04-12 03:31:57", 
+	"2019-04-12 03:31:57", 
+	6);

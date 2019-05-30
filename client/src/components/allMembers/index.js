d13db9bea1d762a68fafb449a268617e3381e554
Namita Shenai <namita.shenai@gmail.com>
2019-05-30 16:04:43 -0700

    Converting it to a Stateful component

--- client/src/components/allMembers/index.js
+++ client/src/components/allMembers/index.js
@@ -1,19 +1,21 @@
-import React from "react"; 
+import React from "react";
 import "./style.css";
 
-function AllMembers({children}) {  
+// Converting the function to a component 
+class AllMembers extends Component {
+  render() {
     return (
       <div className="container mt-4 mb-4">
         <div className="card">
           <h5 id="allmembers-title" className="card-header ">All Members</h5>
           <div className="card-body">
-            <div id="each-member"className="card-columns">
+            <div id="each-member" className="card-columns">
               {children}
             </div>
           </div>
         </div>
       </div>
     );
+  }
 }
-
-export default  AllMembers; 
+export default AllMembers; 

613538cfe2d4467c09b188b8827718fd714e5863
SophieM <47410186+SophM@users.noreply.github.com>
2019-05-30 15:40:43 -0700

    add validation for the add-school form - the parent can't submit the form if all the fields haven't been filled up, add styling to the different buttons on the sign-up form and add 2 buttons - to go back to the first part of the registration when on the kid part and to hide the add-school form if the parent actually doesn't need to add a school to the database

--- client/src/components/add-school/index.js
+++ client/src/components/add-school/index.js
@@ -2,6 +2,8 @@
 import React, { Component } from "react";
 import { FormLabel, FormButton, Dropdown, OptionForDropdown } from "../form";
 import API from "../../utils/API";
+import ErrorMessage from "../errorMessage";
+import "./style.css";
 
 const statesList = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
 
@@ -42,7 +44,8 @@ class AddSchool extends Component {
                     value: ""
                 }
 
-            ]
+            ],
+        hasError: false
     }
 
     handleInputChangeSchool = event => {
@@ -58,31 +61,69 @@ class AddSchool extends Component {
     handleAddSchoolButtonClick = event => {
         event.preventDefault();
 
-        // grab the school info and store them in a variable
-        const schoolData = {
-            name: this.state.schoolInfo[0].value,
-            streetAddress: this.state.schoolInfo[1].value,
-            city: this.state.schoolInfo[2].value,
-            state: this.state.schoolInfo[3].value,
-            zipcode: this.state.schoolInfo[4].value
-        }
+        // the parent has to enter info in every field to be able to submit form
+        if (this.state.schoolInfo[0].value && this.state.schoolInfo[1].value && this.state.schoolInfo[2].value && this.state.schoolInfo[3].value && this.state.schoolInfo[4].value) {
 
-        console.log("school data: ", schoolData);
+            // grab the school info and store them in a variable
+            const schoolData = {
+                name: this.state.schoolInfo[0].value,
+                streetAddress: this.state.schoolInfo[1].value,
+                city: this.state.schoolInfo[2].value,
+                state: this.state.schoolInfo[3].value,
+                zipcode: this.state.schoolInfo[4].value
+            }
 
-        // api call to create the school in the database
-        API.createSchool(schoolData)
-            .then( res => {
-                console.log("School created in DB!");
-                this.props.toUpdateSchoolList();
+            console.log("school data: ", schoolData);
+
+            // api call to create the school in the database
+            API.createSchool(schoolData)
+                .then( res => {
+                    console.log("School created in DB!");
+                    // update the dropdown menu for the schools - method defined on the signUp-form component
+                    this.props.toUpdateSchoolList();
+                })
+                .catch(err => console.log(err))
+
+        // otherwise will get error message that the parent has to fill up the fields
+        } else {
+
+            this.setState({ 
+                hasError: true
             })
-            .catch(err => console.log(err))
+        }
+    }
 
+    handleCloseButtonClick = event => {
+        event.preventDefault();
+
+        this.setState({
+            hasError: false,
+        })
+    }
+
+    resetError = () => {
+        if (this.state.hasError) {
+            setTimeout(() => {
+                this.setState({
+                    hasError: false,
+                })
+            }, 2000)
+        }
     }
 
 
     render() {
+        this.resetError();
         return (
             <div>
+                {(this.state.hasError) ? (
+                    <ErrorMessage
+                        message="Please fill up all the fields!"
+                        handleCloseButtonClick={this.handleCloseButtonClick}
+                    />
+                ) : (
+                    ""
+                )}
                 {this.state.schoolInfo.map((schoolInfo, i) => {
                     if (schoolInfo.for !== "state") {
                         return (
@@ -117,7 +158,15 @@ class AddSchool extends Component {
                 })}
                 <FormButton 
                     nameButton="Add school to the list!"
+                    moreClass="add-school-btn ml-4 mr-3"
+                    icon="fas fa-school"
                     handleButtonClick={this.handleAddSchoolButtonClick}
+                />
+                <FormButton 
+                    nameButton="Nevermind, I found it!"
+                    moreClass="nevermind-btn"
+                    icon=""
+                    handleButtonClick={this.props.toHideAddSchoolForm}
                 />
             </div>
         )
